5516414d1b55c9d6c30a33a49bc8a10aef40feb7
jhadev <joshappeldev@gmail.com>
2019-07-21 16:20:08 -0400

    async spotify

--- liri.js
+++ liri.js
@@ -30,7 +30,7 @@ const dispatch = (command, searchTerm) => {
 };
 
 // spotify function doesn't need to be defined as async because node-spotify-api accepts a callback in its search method.
-const searchSpotify = searchTerm => {
+const searchSpotify = async searchTerm => {
   const spotify = new Spotify(keys.spotify);
   //if there is no song input then default to Lithium
   if (!searchTerm) {
@@ -43,15 +43,14 @@ const searchSpotify = searchTerm => {
     query: searchTerm
   };
 
-  // our callback function to pass into spotify search method which will handle printing data to the console.
-  const action = (err, data) => {
-    // if there's an error stop the code from running
-    if (err) {
-      throw err;
-    }
-    const { items } = data.tracks;
+  try {
+    // await response from spotify
+    const response = await spotify.search(params);
+
+    const { items } = response.tracks;
 
     items.forEach(song => {
+      // artists come back as an array of objects, we can use map to only get the name
       const dataString = `
 Artist(s): ${song.artists.map(artist => artist.name)}
 Song: ${song.name}
@@ -60,11 +59,12 @@ Album: ${song.album.name}
 `;
       // artists come back as an array of objects, we can use map to only get the name
       console.log(dataString);
+      // simplest way to do this, there is definitely a more efficient way.
       logDataToFile('./logs/spotifyLog.txt', dataString);
     });
-  };
-  // call spotify method with the parameters and action to run.
-  spotify.search(params, action);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // axios returns a promise but we can use async await to make our code read more like
